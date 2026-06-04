68d5f3dbae873501337ae4fe758538639e116042
Drew Silcock <drew.silcock@stfc.ac.uk>
2026-06-04 19:08:35 +0100

    Fix Twitter card and add schema-ld data.

--- src/consts.ts
+++ src/consts.ts
@@ -6,6 +6,7 @@ export const SITE: Site = {
   DESCRIPTION:
     "Drew's Dev Blog featuring thoughts and explainers on tech, software engineering, databases, programming languages and more.",
   EMAIL: "031a0c1d2b1f04020c1f12195d101000",
+  TWITTER_HANDLE: "@drewlhsilcock",
   NUM_POSTS_ON_HOMEPAGE: 5,
   NUM_PROJECTS_ON_HOMEPAGE: 3,
 };
--- src/types.ts
+++ src/types.ts
@@ -3,6 +3,7 @@ export type Site = {
   AUTHOR: string;
   DESCRIPTION: string;
   EMAIL: string;
+  TWITTER_HANDLE: string;
   NUM_POSTS_ON_HOMEPAGE: number;
   NUM_PROJECTS_ON_HOMEPAGE: number;
 };
